1cc807926d206814f83ec08d2198bf5eb51318cf
Erick Bernal Navarro <Erick Bernal Navarro>
2016-07-17 00:26:31 -0500

    Add filter

--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -38,5 +38,59 @@ describe('TodoAPI', () => {
       var actualTodos = TodoAPI.getTodos();
       expect(actualTodos).toEqual([]);
     });
+
+    it('should return todo if valid array in localStorage', () => {
+      var todos = [{
+        id: 23,
+        text: 'test all files',
+        completed: false
+      }];
+
+      localStorage.setItem('todos', JSON.stringify(todos));
+      var actualTodos = TodoAPI.getTodos();
+
+      expect(actualTodos).toEqual(todos);
+    });
+  });
+
+  describe('filterTodos', () => {
+    var todos = [{
+      id: 1,
+      text: 'Some text',
+      completed: true
+    },{
+      id: 2,
+      text: 'Other Some text',
+      completed: false
+    },{
+      id: 3,
+      text: 'Text',
+      completed: true
+    }];
+
+    it('should return all items if showCompleted is true', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+      expect(filteredTodos.length).toBe(3);
+    });
+
+    it('should return only the items that are not completed', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, false, '');
+      expect(filteredTodos.length).toBe(1);
+    });
+
+    it('should sort by completed status', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+      expect(filteredTodos[0].completed).toBe(false);
+    });
+
+    it('should filter todos by searchText', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, 'some');
+      expect(filteredTodos.length).toBe(2);
+    });
+
+    it('should return all todos if searchText is empty', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+      expect(filteredTodos.length).toBe(3);
+    });
   });
 });
