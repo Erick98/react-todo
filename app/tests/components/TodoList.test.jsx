8b4a75736583b060fa7da5604bb2f2c605d99743
Erick Bernal Navarro <Erick Bernal Navarro>
2016-07-17 19:44:35 -0500

    Style todo app

--- app/tests/components/TodoList.test.jsx
+++ app/tests/components/TodoList.test.jsx
@@ -12,17 +12,25 @@ describe('TodoList', () => {
     expect(TodoList).toExist();
   });
 
-//  it('should render one Todo component for each todo item', () => {
-//    var todos = [{
-//      id: 1,
-//      text: 'Do something'
-//    }, {
-//      id: 2,
-//      text: 'Do something else'
-//    }];
-//    var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos} />);
-//    var todosComponents =TestUtils.scryRenderedComponentsWithType(TodoList, Todo);
-//
-//    expect(todosComponents.length).toBe(todos.length);
-//  });
+  it('should render one Todo component for each todo item', () => {
+    var todos = [{
+      id: 1,
+      text: 'Do something'
+    }, {
+      id: 2,
+      text: 'Do something else'
+    }];
+    var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos} />);
+    var todosComponents =TestUtils.scryRenderedComponentsWithType(todoList, Todo);
+
+    expect(todosComponents.length).toBe(todos.length);
+  });
+
+  it('should render empty message if no todos', () => {
+    var todos = [];
+    var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos} />);
+    var $el = $(ReactDOM.findDOMNode(todoList));
+
+    expect($el.find('.container__message').length).toBe(1);
+  });
 });
